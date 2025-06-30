04c3de51bac99255b25fcfabb438417b12ff47c6
Gowtham-reddy123 <reddyg026@gmail.com>
2025-06-30 19:44:36 +0530

    comparitive

---
+++ comparision_operators/lessthan_2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a, b;
+    printf("enter the value of a :");
+    scanf("%d",&a);
+    printf("enter the value of b : ");
+    scanf("%d",&b);
+    printf("%d",a<b); // if true it gives 1 and if it is false it gives 0...
+    return 0;
+}
---
+++ comparision_operators/lessthan_or_equal_1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a ;
+    float b;
+    printf("enter the value of a :");
+    scanf("%d",&a);
+    printf("enter the value of b : ");
+    scanf("%f",&b);
+    printf("%d",a<=b); // if true it gives 1 and if it is false it gives 0...
+    return 0;
+}
